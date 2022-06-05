eb0425a2d2b04ad12e69621f95d68aa05bd9cd1f
BrianPelaez <pelaez_brian@hotmail.com>
2022-06-05 13:09:22 -0300

    Se agrega variable de entorno

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // Busca si hay un archivo con variables de entorno .env
 const express = require('express');
 
 const app = express();
@@ -9,6 +10,8 @@ app.get('/', (req, res) => {
     res.status(200).send("Hola Mundo!");
 });
 
-app.listen(4000, () => {
-    console.log("Escuchando en el puerto 4000");
+const PORT = process.env.PORT || 4000 // Para acceder a las variables de entorno .env o hardcodear puerto 4000
+
+app.listen(PORT, () => {
+    console.log(`Escuchando en el puerto ${PORT}`);
 }) // Para que el server empiece a escuchar por un puerto
